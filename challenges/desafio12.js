4a871ee190ed461cf3ecfac730b17deb69f56097
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-11-23 20:13:39 -0300

    Resolve erros lint

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -4,13 +4,13 @@ db.produtos.updateMany(
     valoresNutricionais: {
       $each: [],
       $sort: { percentual: -1 },
-    }
-    }
+    },
+    },
 
-  }
+  },
 );
 
 db.produtos.find(
   {},
-  { _id: 0, nome: 1, valoresNutricionais: 1 }
+  { _id: 0, nome: 1, valoresNutricionais: 1 },
 );
